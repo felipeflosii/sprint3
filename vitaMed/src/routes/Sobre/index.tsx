c45a9331d67d78ad4fdd0cd709d7446bbd2a0b89
felipeflosii <luizfelipeflosi@gmail.com>
2025-10-01 12:19:15 -0300

    chore: configuração do Tailwind CSS e PostCSS

--- vitaMed/src/routes/Sobre/index.tsx
+++ vitaMed/src/routes/Sobre/index.tsx
@@ -4,11 +4,10 @@ const Sobre: FC = () => {
   return (
     <main>
       <section>
-        <h2>Sobre o Projeto</h2>
+        <h2 className="text-3xl font-bold text-blue-600">Sobre o Projeto</h2>
         <p>
           O Projeto PHC visa melhorar a saúde digital, reduzindo o absenteísmo e
           facilitando o acesso aos serviços digitais de saúde.
-          
         </p>
       </section>
 
@@ -16,14 +15,22 @@ const Sobre: FC = () => {
         <div className="card">
           <i>🧑‍💻</i>
           <h3>Equipe</h3>
-          <p>Nossa equipe é composta por profissionais dedicados à inovação na saúde digital.</p>
+          <p>
+            Nossa equipe é composta por profissionais dedicados à inovação na
+            saúde digital.
+          </p>
         </div>
         <div className="card">
           <i>⚙️</i>
           <h3>Tecnologia</h3>
-          <p>Utilizamos tecnologias modernas para soluções eficientes e acessíveis.</p>
+          <p>
+            Utilizamos tecnologias modernas para soluções eficientes e
+            acessíveis.
+          </p>
         </div>
       </section>
     </main>
   );
-}
+};
+
+export default Sobre;
