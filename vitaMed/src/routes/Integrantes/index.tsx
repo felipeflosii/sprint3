096b20e95ec57167f19d03aa08b842cffd2e6167
thubrito <britoarthur150@gmail.com>
2025-10-22 20:35:43 -0300

    feat: atualização do status do projeto de 'Em Desenvolvimento' para 'Versão Final'

--- vitaMed/src/routes/Integrantes/index.tsx
+++ vitaMed/src/routes/Integrantes/index.tsx
@@ -130,7 +130,7 @@ const Integrantes: React.FC = () => {
                 <div className="flex justify-between">
                   <span className="text-gray-600">Status:</span>
                   <span className="bg-green-100 text-green-800 px-2 py-1 rounded-full text-sm font-semibold">
-                    Em Desenvolvimento
+                    Versão Final
                   </span>
                 </div>
               </div>
