257c35a81e9e63d98b2a411885acfbb65e661c33
felipeflosii <luizfelipeflosi@gmail.com>
2025-10-22 17:45:55 -0300

    feat: modernizando cabeçalho e menu e deixando mais fluido a troca de páginas

--- vitaMed/src/App.tsx
+++ vitaMed/src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Cabecalho from './components/Cabecalho/Cabecalho';
-import Menu from './components/Menu/Menu';
 import Rodape from './components/Rodape/Rodape';
 
 const App: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Cabecalho />
-      <Menu />
       <main className="flex-grow">
-        <Outlet /> {/* Aqui as rotas serão renderizadas */}
+        <Outlet /> 
       </main>
       <Rodape />
     </div>
